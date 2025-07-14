012e7110f2bef2c3d809e63fa6f0844176526bba
kumaraguhan <147791603+kumaraguhan@users.noreply.github.com>
2025-07-14 18:28:20 +0530

    Add Detail View for image (#5)

--- images/models.py
+++ images/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from django.conf import settings
 from django.utils.text import slugify
 class Image(models.Model):
@@ -21,6 +22,9 @@ class Image(models.Model):
         if not self.slug:
             self.slug = slugify(self.title)
         super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse('images:detail', args=[self.id, self.slug])
     
     def __str__(self):
         return self.title
