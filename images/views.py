b210aff28b235105f7296266ee5a6fc8d3a16a0d
kumaraguhan <147791603+kumaraguhan@users.noreply.github.com>
2025-07-16 16:00:17 +0530

    Branch ubuntu (#14)
    Use Redis for Storing and retrieving data for faster process

--- images/views.py
+++ images/views.py
@@ -34,10 +34,15 @@ from django.shortcuts import get_object_or_404
 from .models import Image
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
+     
+    # increment total image views by 1
+    total_views = r.incr(f'image:{image.id}:views')
+    
     return render(request,
                   'images/image/detail.html',
                   {'section': 'images',
-                   'image': image})
+                   'image': image,
+                    'total_views': total_views})
 
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST
@@ -93,3 +98,11 @@ def image_list(request):
     return render(request,
                   'images/image/list.html',
                    {'section': 'images', 'images': images})
+
+
+import redis
+from django.conf import settings
+# connect to redis
+r = redis.Redis(host=settings.REDIS_HOST,
+                port=settings.REDIS_PORT,
+                db=settings.REDIS_DB)
